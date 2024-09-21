45112ad21a13b9f0180b2bfd1dfa85875d9ff803
RamandeepNahar <ramandeep.s.nahar@gmail.com>
2024-09-21 13:39:08 -0400

    added the function validateInput() to handle the user input

--- app/src/main/java/edu/temple/baseassignment/MainActivity.kt
+++ app/src/main/java/edu/temple/baseassignment/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var editTextPassword: EditText
     lateinit var editTextPassword2: EditText
     lateinit var myButton: Button
+    lateinit var spinner: Spinner
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,17 +30,19 @@ class MainActivity : AppCompatActivity() {
         editTextPassword = findViewById(R.id.editTextTextPassword)
         editTextPassword2 = findViewById(R.id.editTextTextPassword2)
         myButton = findViewById(R.id.saveInfo)
+        spinner = findViewById(R.id.programField)
 
-        val spinner = findViewById<Spinner>(R.id.programField)
         val program = arrayOf("Please select your program", "Information Science", "Computer Science", "Math and CS", "Data Science", "Other")
         val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, program)
 
         spinner.adapter = adapter
 
         myButton.setOnClickListener {
-            displayMessageTextView.text = "Welcome To The App, ${nameEditText.text}"
+            validateInput()
         }
+    }
 
+    private fun validateInput(){  // validate the user input
 
     }
 }
